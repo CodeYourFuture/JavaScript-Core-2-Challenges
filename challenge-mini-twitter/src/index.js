3e784e93a8d1ca3b5ddb49b3715dfbd5c1fc9979
Cecilia <earwen1986@gmail.com>
2020-12-17 12:04:25 +0000

    Completed mini twitter

--- challenge-mini-twitter/src/index.js
+++ challenge-mini-twitter/src/index.js
@@ -5,19 +5,32 @@ saveBtn.addEventListener("click", function (event) {
 	event.preventDefault();
 	let tweet = document.querySelector("textarea");
 	if (tweet.value.length <= 280) {
-		// Create tweet
-		let container = document.createElement("div");
+		
+		let container = document.createElement("article");
 		let tweetArea = document.createElement("p");
-		tweetArea.innerHTML = tweet.value;
+		// deal with handle if present
+		if (tweet.value[0] === '@') {
+			let userName = tweet.value.split(" ")[0].slice(1);
+			let tweetText = tweet.value.split(" ").slice(1).join(" ");
+			tweetArea.innerHTML = tweetText;
+			let handle = `<a href="www.twitter.com/${userName}">@${userName}</a>`
+			container.innerHTML = handle;
+		} else {
+			container.innerHTML = "<a></a>"
+			tweetArea.innerHTML = tweet.value;
+		}
 		container.append(tweetArea);
 		tweet.value = "";
+		document.querySelector("#charNum").innerHTML = "Characters: 0"
 		// Create delete button
 		let delBtn = document.createElement("button")
 		delBtn.innerHTML = "Delete";
+		delBtn.classList = "delBtn";
 		delBtn.addEventListener("click", function(){
 			this.parentNode.parentNode.removeChild(this.parentNode);
 		})
 		container.append(delBtn);
+
 		document.querySelector("#tweets").append(container);
 	}
 });
