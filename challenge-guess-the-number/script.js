cb845d6d604f92b400a52c0ef7266b6c342f0e83
Miss <emesquitta73@gmail.com>
2021-02-09 18:53:05 +0000

    small changes made

--- challenge-guess-the-number/script.js
+++ challenge-guess-the-number/script.js
@@ -1,12 +1,30 @@
+function guessNumber(){
 let randomNumber = Math.floor(Math.random() * 100 + 1);
+//let output = document.getElementsByClassName("final-output");
+let output = document.getElementById("outputNum");
 
-function guessNumber() {
-  //Collect input from the user
-  let guess = document.querySelector(".inputs-Values").value;
+let inputNum = document.getElementById("inputValues").value; //Collect input from the user
+//console.log(inputNum);
+if (inputNum < 1 && inputNum >100){
+  output.innerHTML = "Please enter a number between 1 and 100";
+}
+else{
 
-// Comment for conflict
+  if (inputNum > randomNumber){
+        output.innerHTML = "Your guess is too high! Try again";
+       }else if(inputNum < randomNumber){
+         output.innerHTML = "Your guess is too low! Try again";
+        }
+        else{
+      output.innerHTML= "Guess is correct. You Win!";
+    }
 
-I am queen!
+}
+
+      
+}
+
+
   //If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
 
   //If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
@@ -14,7 +32,7 @@ I am queen!
   //If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
 
   //If the user has guessed the random number correctly print out the randomNumber with a message "Guess is correct. You win!"
-}
+
 
 // For this task we will be making a "New Game" button function which will reset our game,
 // Once the user clicks on this button the user will have new random number to guess
