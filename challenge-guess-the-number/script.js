ca1e5360b2d2d181ef6828080730cea802fdef5b
samia-pk <samiaarshad92@gmail.com>
2021-02-10 16:22:35 +0000

    reset random numbers, set number of tries

--- challenge-guess-the-number/script.js
+++ challenge-guess-the-number/script.js
@@ -23,12 +23,19 @@ function guessNumber() {
 // Once the user clicks on this button the user will have new random number to guess
 // 1. Reset the values inside the body of the function
 // 2. Attach our new game button using an event listener to the .btnNewGame button
+
 function newGame() {
-  //Your code here
-  //Reset randomNumber
-  //Reset users input field
-  //Reset tries, and triesTaken by the user
+
+ finalOutPut.innerHTML = "Please enter a number  between 1 and 100";
+ GamePlaying = false;
+  let randomNumber = Math.floor(Math.random() * 100 + 1);                 //Reset randomNumber
+  let guess = (document.querySelector(".inputs-Values").value = "");      //Reset users input field
+  
+  numberOfTries = 7;                                                      //Reset tries, and triesTaken by the user
+  numberOfTriesTaken = 0;
+  NumberOfTriesEl.innerHTML = `Number of tries: ${numberOfTries}`;
 }
+
 
 //keyboard exception
 function keyBoardEvents(e) {
